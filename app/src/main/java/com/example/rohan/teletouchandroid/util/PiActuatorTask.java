8ff9f460ef9ba004b2156c828378fb60cda85655
Rohan Jadvani <rj1030@gmail.com>
2017-05-02 18:06:51 -0400

    Pass full dictionary on socket writes.

--- app/src/main/java/com/example/rohan/teletouchandroid/util/PiActuatorTask.java
+++ app/src/main/java/com/example/rohan/teletouchandroid/util/PiActuatorTask.java
@@ -1,6 +1,7 @@
 package com.example.rohan.teletouchandroid.util;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -21,15 +22,39 @@ public class PiActuatorTask extends AsyncTask<Integer, Void, Void> {
         this.port = port;
     }
 
+    /**
+     * Builds a string representing a Python dictionary mapping locations on the hand to pressure
+     * intensities at those locations.
+     *
+     * @param handId    handId that is currently being touched
+     * @param intensity intensity at that position
+     * @return String of Python-interpretable dictionary with hand to pressure mapping.
+     */
+    public String buildPressureDict(int handId, int intensity) {
+        String result = "{ ";
+        int startingId = 1;
+        int endingId = 20;
+
+        // Build dictionary with all intensities
+        for (int id = startingId; id <= endingId; id++) {
+            int handIntensity = (handId == id) ? intensity : 0;
+            result += id + ": " + handIntensity;
+            result += (id < endingId) ? ", " : " }";
+        }
+
+        return result;
+    }
+
     @Override
     protected Void doInBackground(Integer... params) {
         int id = params[0];
         int intensity = params[1];
+        String pressureDict = buildPressureDict(id, intensity);
 
         try {
             Socket socket = new Socket(hostAddress, port);
             DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-            DOS.writeUTF(id + "," + intensity);
+            DOS.writeUTF(pressureDict);
             socket.close();
         } catch (UnknownHostException e) {
             e.printStackTrace();
